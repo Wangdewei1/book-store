9ebffc9ff502596fd2ef6afdd195011f28f14d63
wangddwei <1482466683@qq.com>
2023-06-05 20:07:09 +0800

    改变订单测试变量

--- book-store03/book/src/com/itwang/book/utils/OrderConst.java
+++ book-store03/book/src/com/itwang/book/utils/OrderConst.java
@@ -3,5 +3,5 @@ package com.itwang.book.utils;
 public class OrderConst {
     public static final String ORDER_LIST_NAME = "orders";
 
-    public static final String ORDER_GIT_TEST = "test";
+    public static final String ORDER_GIT_TEST = "TEST";
 }
