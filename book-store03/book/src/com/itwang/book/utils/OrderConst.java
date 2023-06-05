b2aa0a43cb5968650f63984298f66200e8db4422
wangddwei <1482466683@qq.com>
2023-06-05 19:54:06 +0800

    改变常量

--- book-store03/book/src/com/itwang/book/utils/OrderConst.java
+++ book-store03/book/src/com/itwang/book/utils/OrderConst.java
@@ -1,5 +1,5 @@
 package com.itwang.book.utils;
 
 public class OrderConst {
-    public static final String ORDER_LIST_NAME = "orderList";
+    public static final String ORDER_LIST_NAME = "orders";
 }
